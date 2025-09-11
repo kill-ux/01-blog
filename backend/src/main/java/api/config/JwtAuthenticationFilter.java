d75411968c9e808f95536cf2e14b1f24059ca097
kill-ux || Mustapha Boutoub <mustaphaboutoubdev@gmail.com>
2025-09-11 14:49:39 +0100

    token

---
+++ backend/src/main/java/api/config/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package api.config;
+
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import api.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(
+            HandlerExceptionResolver handlerExceptionResolver,
+            JwtService jwtService,
+            UserDetailsService userDetailsService) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String jwt = authHeader.substring(7);
+            final String nickname = jwtService.
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+    }
+
+}
--- backend/src/main/java/api/service/JwtService.java
+++ backend/src/main/java/api/service/JwtService.java
@@ -1,5 +1,32 @@
 package api.service;
 
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 public class JwtService {
-    
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    public String extractNickname(String token) {
+        
+    }
+
+    public <T> T extractClaim(String token,Function<Claims , T> claimsResolver) {
+        final Claims claims = 
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts
+            .parser()
+            .verifyWith(getSignInKey)
+            
+    }
+
 }
