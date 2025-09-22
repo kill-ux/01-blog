2843d98669a90b0f23493fd7e80f4b312cb0d343
kill-ux <mustaphaboutoubdev@gmail.com>
2025-09-22 10:33:59 +0100

    j

--- backend/src/test/java/api/backend/BackendApplicationTests.java
+++ backend/src/test/java/api/backend/BackendApplicationTests.java
@@ -1,13 +1,29 @@
 package api.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@WebMvcTest
 class BackendApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void shouldReturnAllUsers() throws Exception {
+		this.mockMvc
+		.perform(MockMvcRequestBuilders.get("/api/users"))
+		.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
