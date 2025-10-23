01ba2265e15c2a52732362061388a7cb594f68a9
killux <mustaphaboutoubdev@gmail.com>
2025-10-23 13:29:10 +0100

    j

--- frontend/src/app/features/user/services/websocket.ts
+++ frontend/src/app/features/user/services/websocket.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Client } from '@stomp/stompjs'
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Notifications } from '../notifications/notifications';
+
+import SockJS  from 'sockjs-client';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class Websocket {
-  
+	private stompClient: Client
+	private notificationSubject = new BehaviorSubject<Notifications | null>(null)
+	public notifications$: Observable<Notifications | null> = this.notificationSubject.asObservable();
+
+	private connectedSubject = new BehaviorSubject<boolean>(false)
+	public connected$: Observable<boolean> = this.connectedSubject.asObservable();
+
+	constructor(){
+		this.initializeWebSocketConnection()
+	}
+
+	private initializeWebSocketConnection() {
+		const socket = SockJS('http://localhost:8080/ws')
+	}
+
 }
