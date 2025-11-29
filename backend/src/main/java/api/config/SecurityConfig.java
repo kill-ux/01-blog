a9f57b8c0c3a635b9b7c64cf241cbf4e2ecf44a2
killua <mustaphaboutoubdev@gmail.com>
2025-11-29 18:05:56 +0100

    hh

--- backend/src/main/java/api/config/SecurityConfig.java
+++ backend/src/main/java/api/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 
 package api.config;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
+import java.time.Duration;  
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
